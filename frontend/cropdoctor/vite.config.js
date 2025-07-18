d39326411175e912741e81ca1185d692cd477303
yash565-git0 <012yash789@gmail.com>
2025-07-18 14:46:42 +0530

    changed vite config file

--- frontend/cropdoctor/vite.config.js
+++ frontend/cropdoctor/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  plugins: [ tailwindcss(),react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/onnxruntime-web/dist/*.wasm',
+          dest: '.'
+        }
+      ]
+    })
+  ],
 })
