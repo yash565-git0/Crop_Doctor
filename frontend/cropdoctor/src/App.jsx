4a7bcd311ae65fdb0a22e53484fc88b1b3f2cec6
yash565-git0 <012yash789@gmail.com>
2025-07-18 15:23:18 +0530

    minor bug update

--- frontend/cropdoctor/src/App.jsx
+++ frontend/cropdoctor/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './context/AuthContext'; // Import the provider
+import { AuthProvider } from './context/AuthContext.jsx'; // Import the provider
 
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import DiseaseDetection from './components/Diseasedetection/Diseasedetection';
+
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
+import DiseaseDetection from './components/Diseasedetection/Diseasedetection.jsx';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Toaster />
-        <Navbar />
+        
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
--- frontend/cropdoctor/src/components/Login/Login.jsx
+++ frontend/cropdoctor/src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthContext'; // Import the context
+import { AuthContext } from '../../context/AuthContext.jsx'; // Import the context
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
